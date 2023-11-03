f83d128f608cabedca968532778533e01ea6ca21
Stavros Kaparelos <skaparelos@gmail.com>
2023-11-03 15:29:36 +0200

    update

--- index.ts
+++ index.ts
@@ -5,25 +5,25 @@ import * as gcp from "@pulumi/gcp";
 // Get the GCP project config
 const projectId = 'deleteme-403817'
 const location = 'us-central1'
-const prefix = 'peacock-render-faas'
+const prefix = 'test'
 const customRuntimeEnvironmentRegistry = `${prefix}-artifact-registry`
 const customRuntimeEnvironmentName = `${prefix}-image`
 const customRuntimeRepositoryName = `${prefix}-repository`
-const currentImageVersion = 'v0.0.1' //this needs to be incremented on each deployment currently
 
 
 // Create a Google Artifact Registry repository to store Docker images
-const repository = new gcp.artifactregistry.Repository("app-repository", {
+const repository = new gcp.artifactregistry.Repository(customRuntimeEnvironmentRegistry, {
     location,
-    repositoryId: "app-repository",
+    repositoryId: customRuntimeRepositoryName,
     format: "DOCKER",
 });
+
 
 // Get registry info (creds and endpoint).
 
 const renderFaasDockerImageName = repository.name.apply(
     (name) =>
-      `${location}-docker.pkg.dev/${projectId}/${name}/${customRuntimeEnvironmentName}:${currentImageVersion}`,
+      `${location}-docker.pkg.dev/${projectId}/${name}/${customRuntimeEnvironmentName}:latest`,
   )
 
 // Build and publish the app image.
@@ -35,16 +35,16 @@ const image = new docker.Image("node-app-image", {
 });
 
 // Create a Cloud Run service that uses the Docker image
-const service = new gcp.cloudrun.Service("app-service", {
-    location: "us-central1",
-    template: {
-        spec: {
-            containers: [{
-                image: image.imageName,
-            }],
-        },
-    },
-});
+// const service = new gcp.cloudrun.Service("app-service", {
+//     location: "us-central1",
+//     template: {
+//         spec: {
+//             containers: [{
+//                 image: image.imageName,
+//             }],
+//         },
+//     },
+// });
 
 // Export the URL of the deployed service
-export const url = service.statuses[0].url;
+// export const url = service.statuses[0].url;
