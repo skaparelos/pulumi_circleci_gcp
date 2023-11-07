73cc137bc0123af7ca770e76d0b5cb7b52370de9
Stavros Kaparelos <skaparelos@gmail.com>
2023-11-07 19:10:42 +0200

    trigger

--- infra/shared/index.ts
+++ infra/shared/index.ts
@@ -1,12 +1,11 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as docker from "@pulumi/docker";
+// import * as pulumi from "@pulumi/pulumi";
+// import * as docker from "@pulumi/docker";
 import * as gcp from "@pulumi/gcp";
 
 // Get the GCP project config
-const projectId = 'deleteme-403817'
+// const projectId = 'deleteme-403817'
 const location = 'us-central1'
-const prefix = 'test'
-
+// const prefix = 'test'
 
 
 const repository = new gcp.artifactregistry.Repository(
