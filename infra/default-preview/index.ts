7a6092ec5b0edc5db21f42cbfa6ae8e2b22b6aab
Stavros Kaparelos <skaparelos@gmail.com>
2023-11-08 12:18:04 +0200

    default-preview & check merge destroy

---
+++ infra/default-preview/index.ts
@@ -0,0 +1,49 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as docker from "@pulumi/docker";
+import * as gcp from "@pulumi/gcp";
+
+// Get the GCP project config
+const projectId = 'deleteme-403817'
+const location = 'us-central1'
+const prefix = 'test'
+let customRuntimeEnvironmentName = `${prefix}-image`
+
+const config = new pulumi.Config()
+const branchName = config.require('branch')?.toLowerCase()
+console.log("branch name=", branchName)
+
+
+const sharedStack = new pulumi.StackReference("skaparelos/pulumi-tests/shared");
+const repositoryName = sharedStack.getOutput("repositoryName");
+
+
+const renderFaasDockerImageName = pulumi.all([repositoryName, "us-central1", projectId])
+  .apply(([repoName, repoLocation, projId]) =>
+    `${repoLocation}-docker.pkg.dev/${projId}/${repoName}/${customRuntimeEnvironmentName}:default-preview`
+  );
+
+const image = new docker.Image(customRuntimeEnvironmentName, {
+  build: {
+    context: "../../backend1/",
+    platform: 'linux/amd64',
+  },
+  imageName: renderFaasDockerImageName,
+});
+
+// Create a Cloud Run service that uses the Docker image
+const service = new gcp.cloudrun.Service(`${prefix}-service-default-preview`, {
+  location: "us-central1",
+  template: {
+    spec: {
+      containers: [{
+        image: image.imageName,
+      }],
+    },
+  },
+});
+
+// Export the URL of the deployed service
+exports.url = service.statuses[0].url;
+
+
+
