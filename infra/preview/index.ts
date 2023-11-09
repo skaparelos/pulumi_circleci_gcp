d4774098084623445b79397f878764bb6f76d5b2
Stavros Kaparelos <skaparelos@gmail.com>
2023-11-09 16:51:03 +0200

    updated infra scripts

--- infra/preview/index.ts
+++ infra/preview/index.ts
@@ -2,16 +2,17 @@ import * as pulumi from "@pulumi/pulumi";
 import * as docker from "@pulumi/docker";
 import * as gcp from "@pulumi/gcp";
 
+
 // Get the GCP project config
 const projectId = 'deleteme-403817'
 const location = 'us-central1'
-const prefix = 'test'
-
-let customRuntimeEnvironmentName = `${prefix}-image`
 
 const config = new pulumi.Config()
+const serviceName = config.require('serviceName')?.toLowerCase() ?? 'test'
 const branchName = config.require('branch')?.toLowerCase()
 console.log("branch name=", branchName)
+
+const customRuntimeEnvironmentName = `${serviceName}-image`
 
 // gets repository from 'shared' stack
 const sharedStack = new pulumi.StackReference("skaparelos/pulumi-tests/shared");
@@ -25,14 +26,14 @@ const renderFaasDockerImageName = pulumi.all([repositoryName, "us-central1", pro
 
 const image = new docker.Image(customRuntimeEnvironmentName, {
   build: {
-    context: "../../backend1/",
+    context: `../../${serviceName}/`,
     platform: 'linux/amd64',
   },
   imageName: renderFaasDockerImageName,
 });
 
 // Create a Cloud Run service that uses the Docker image
-const service = new gcp.cloudrunv2.Service(`${prefix}-service-${branchName}-preview`, {
+const service = new gcp.cloudrunv2.Service(`${serviceName}-service-${branchName}-preview`, {
   ingress: "INGRESS_TRAFFIC_ALL",
   location: "us-central1",
   template: {
